43ecea526554bc15edaaaf6a7377c31dac84ab1d
Bumble <63314839+Kariem816@users.noreply.github.com>
2021-08-09 18:22:08 +0200

    Update assi.ino

--- assi.ino
+++ assi.ino
@@ -3,36 +3,44 @@ Servo scanservo;
 #define trig 2
 #define echo A1 
 
-float realdis(int trigpin,int echopin){//ultrasonic function to measure distance 
-digitalWrite(trigpin,HIGH);
+void turnServo(int angle, Servo mys){
+  for (int pos = 0; pos <= angle; pos += 1) {
+      mys.write(pos);
+      delay(15);
+    }
+}
+
+float realdis(int trigpin, int echopin){//ultrasonic function to measure distance 
+digitalWrite(trigpin, HIGH);
 delayMicroseconds(10);
-digitalWrite(trigpin,LOW);
-return (pulseIn(echopin,HIGH))*343.0/10000.0/2.0;
+digitalWrite(trigpin, LOW);
+int duration = pulseIn(echopin, HIGH);
+return (duration * 343.0 / 20000.0);
 delay(60);
 }
 
-
-void servo_scan(int leftangle,float leftdist,int backangle,float backdist,int rightangle,float rightdist,Servo mys){//function to set the servo for 3 entered angles to measure each distance 
-  mys.write(leftangle);
+void servo_scan(int leftangle, float leftdist, int backangle, float backdist, int rightangle, float rightdist, Servo mys){//function to set the servo for 3 entered angles to measure each distance 
+  turnServo(leftangle, mys);
   delay(60);
   leftdist = realdis(trig, echo);
-  mys.write(backangle);
+  turnServo(backangle, mys);
   delay(60);
   backdist = realdis(trig, echo);
-  mys.write(rightangle);
+  turnServo(rightangle, mys);
   delay(60);
   rightdist = realdis(trig, echo);
-  }
+}
 
 void setup() {
 scanservo.attach(3);
-pinMode(trig,OUTPUT);
-pinMode(echo,INPUT);
+pinMode(trig, OUTPUT);
+pinMode(echo, INPUT);
 }
+
 void loop() {
 float backdis;
 float rightdis;
 float leftdis;
-servo_scan(45,leftdis,90,backdis,135,rightdis,scanservo);
-delay(60);      
+servo_scan(45, leftdis, 90, backdis, 135, rightdis, scanservo);
+delay(60); 
 }
